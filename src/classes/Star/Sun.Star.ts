2e9bc9b61b2153106bbae882ee7c4c669961c7f0
xTCry <wolt6@mail.ru>
2020-02-25 22:16:58 +0300

    Added characteristics of the sun

--- src/classes/Star/Sun.Star.ts
+++ src/classes/Star/Sun.Star.ts
@@ -3,6 +3,10 @@ import Star from './Star';
 export default class Sun_Star extends Star {
     name = 'Sun';
 
+    size = 696340;
+    luminosity = 1n;
+    surrfaceTemperature = 5778;
+
     public async Load() {
         // const items = Object.values(Items).map(e => new e());
         // this.AddItem(items);
